5b7ffb2ddadbae64a36097b8cc45e2e61af457ac
jinkim31 <87002847+jinkim31@users.noreply.github.com>
2023-03-09 14:44:14 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,17 @@
 
 opencv_version="4.6.0"
 cuda_arch="8.6"
+build_type="DEBUG"
+
+printf 'Install Opencv %s with CUDA arch %s (y/n)? ' $opencv_version $cuda_arch
+read answer
+if [ "$answer" != "${answer#[Nn]}" ] ;then
+    exit 0
+fi
 
 sudo apt-get update
 
-echo "Installing opencv dependencies"
+echo "==========Installing opencv dependencies=========="
 sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 sudo apt-get install -y python3.8-dev python-dev python-numpy python3-numpy
@@ -13,15 +20,14 @@ sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev li
 sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 sudo apt-get install -y curl
 
-echo "Downloading OpenCV"
-cd ~/Downloads
+echo "==========Downloading OpenCV=========="
 curl -L https://github.com/opencv/opencv/archive/${opencv_version}.zip -o opencv-${opencv_version}.zip
 curl -L https://github.com/opencv/opencv_contrib/archive/${opencv_version}.zip -o opencv_contrib-${opencv_version}.zip
 unzip opencv-${opencv_version}.zip
 unzip opencv_contrib-${opencv_version}.zip
 rm opencv-${opencv_version}.zip opencv_contrib-${opencv_version}.zip
 
-echo "Building OpenCV"
+echo "==========Building OpenCV=========="
 cd opencv-${opencv_version}/
 mkdir release
 cd release
@@ -44,11 +50,22 @@ cmake \
 -D BUILD_TESTS=OFF \
 -D BUILD_PERF_TESTS=OFF \
 -D BUILD_EXAMPLES=OFF \
--D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_BUILD_TYPE=${build_type} \
 -D OPENCV_ENABLE_NONFREE=ON \
 -D BUILD_TIFF=ON \
--D CMAKE_INSTALL_PREFIX=/usr/local/opencv-${opencv_version} ..
+-D CMAKE_INSTALL_PREFIX=/usr/local/opencv-${opencv_version}-release ..
 make -j$(nproc)
 
-echo "Installing OpenCV"
+printf 'Build completed and will be install at /usr/local/opencv-%s. continue(y/n)? ' $opencv_version $opencv_version
+read answer
+if [ "$answer" != "${answer#[Nn]}" ] ;then 
+    exit 0
+fi
+
+echo "==========Installing OpenCV=========="
 sudo make install
+
+echo "==========Adding user to video group=========="
+sudo adduser $USER video
+
+echo "==========Done!=========="
